3eea9f2b8aec5a3ef51ba8dc179d5121d8038746
camaraguilherme <camaraguilherme148@gmail.com>
2026-04-28 13:02:47 -0300

    começando python

---
+++ ex001/desafio 3/desafio 3.py
@@ -0,0 +1,5 @@
+a= input('Primeiro número: \n')
+b = input('Segundo número: \n')
+
+
+print('A soma é', float(a) + float(b) ,'.')
---
+++ ex001/ex001.py
@@ -0,0 +1,9 @@
+
+print('Olá, mundo\n')
+print(7+4)
+
+nome = input('Qual seu nome?\n')
+idade = input('Qual a sua idade?\n')
+peso = input('Qual seu peso?\n')
+
+print(nome, idade, peso)
